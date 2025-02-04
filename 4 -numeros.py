501e4d99466d0a6b074c712b213dc5f0a5507d85
AnahiPinto7 <anahipinto194@gmail.com>
2025-02-03 21:01:24 -0500

    Add files via upload

---
+++ 4 -numeros.py
@@ -0,0 +1,15 @@
+numero=3
+decimales=5.5
+operacion=9+1
+
+print(9 + 1)
+print(9 - 1)
+print(9 * 1)
+print(9 / 6)# con decimales
+print(9 // 2)# sin decimales
+print(9 ** 4)# eleva a la potencia 1
+
+
+suma=numero+4
+
+suma +=3 # suma=suma+3
